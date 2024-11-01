d1f923ee8835b759cec9bebb8ef6cdf7b95a712f
Thing-han, Lim <15379156+potsrevennil@users.noreply.github.com>
2024-11-01 16:52:06 +0800

    CI: Adjust the tests script and CI action according to the new build system
    `tests all` option `--opt` became a choice flag instead of boolean flag,
    for determining whether to build opt/no-opt or both implementation at once.
    
    CI is refactored accordingly.
    
    Signed-off-by: Thing-han, Lim <15379156+potsrevennil@users.noreply.github.com>

--- test/acvp_client.py
+++ test/acvp_client.py
@@ -9,16 +9,19 @@
 
 import json
 import subprocess
+import sys
 
+suffix = f"_{sys.argv[1]}" if len(sys.argv) > 1 else ""
 acvp_dir = "test/acvp_data"
 acvp_keygen_json = f"{acvp_dir}/acvp_keygen_internalProjection.json"
 acvp_encapDecap_json = f"{acvp_dir}/acvp_encapDecap_internalProjection.json"
 
-with open(acvp_keygen_json, 'r') as f:
+with open(acvp_keygen_json, "r") as f:
     acvp_keygen_data = json.load(f)
 
-with open(acvp_encapDecap_json, 'r') as f:
+with open(acvp_encapDecap_json, "r") as f:
     acvp_encapDecap_data = json.load(f)
+
 
 def get_acvp_binary(tg):
     """Convert JSON dict for ACVP test group to suitable ACVP binary."""
@@ -29,14 +32,22 @@ def get_acvp_binary(tg):
     }
     level = parameterSetToLevel[tg["parameterSet"]]
     basedir = f"./test/build/mlkem{level}/bin"
-    acvp_bin = f"acvp_mlkem{level}"
+    acvp_bin = f"acvp_mlkem{level}{suffix}"
     return f"{basedir}/{acvp_bin}"
 
+
 def run_encapDecap_test(tg, tc):
-    print(f"Running encapDecap test case {tc['tcId']} ({tg['function']}) ... ", end='')
+    print(f"Running encapDecap test case {tc['tcId']} ({tg['function']}) ... ", end="")
     if tg["function"] == "encapsulation":
         acvp_bin = get_acvp_binary(tg)
-        acvp_call = [ acvp_bin, "encapDecap", "AFT", "encapsulation", f"ek={tc['ek']}", f"m={tc['m']}" ]
+        acvp_call = [
+            acvp_bin,
+            "encapDecap",
+            "AFT",
+            "encapsulation",
+            f"ek={tc['ek']}",
+            f"m={tc['m']}",
+        ]
         result = subprocess.run(acvp_call, encoding="utf-8", capture_output=True)
         if result.returncode != 0:
             print("FAIL!")
@@ -45,7 +56,7 @@ def run_encapDecap_test(tg, tc):
             exit(1)
         # Extract results and compare to expected data
         for l in result.stdout.splitlines():
-            (k,v) = l.split("=")
+            (k, v) = l.split("=")
             if v != tc[k]:
                 print("FAIL!")
                 print(f"Mismatching result for {k}: expected {tc[k]}, got {v}")
@@ -53,7 +64,14 @@ def run_encapDecap_test(tg, tc):
         print("OK")
     elif tg["function"] == "decapsulation":
         acvp_bin = get_acvp_binary(tg)
-        acvp_call = [ acvp_bin, "encapDecap", "VAL", "decapsulation", f"dk={tg['dk']}", f"c={tc['c']}" ]
+        acvp_call = [
+            acvp_bin,
+            "encapDecap",
+            "VAL",
+            "decapsulation",
+            f"dk={tg['dk']}",
+            f"c={tc['c']}",
+        ]
         result = subprocess.run(acvp_call, encoding="utf-8", capture_output=True)
         if result.returncode != 0:
             print("FAIL!")
@@ -62,17 +80,18 @@ def run_encapDecap_test(tg, tc):
             exit(1)
         # Extract results and compare to expected data
         for l in result.stdout.splitlines():
-            (k,v) = l.split("=")
+            (k, v) = l.split("=")
             if v != tc[k]:
                 print("FAIL!")
                 print(f"Mismatching result for {k}: expected {tc[k]}, got {v}")
                 exit(1)
         print("OK")
 
+
 def run_keyGen_test(tg, tc):
-    print(f"Running keyGen test case {tc['tcId']} ... ", end='')
+    print(f"Running keyGen test case {tc['tcId']} ... ", end="")
     acvp_bin = get_acvp_binary(tg)
-    acvp_call = [ acvp_bin, "keyGen", "AFT", f"z={tc['z']}", f"d={tc['d']}" ]
+    acvp_call = [acvp_bin, "keyGen", "AFT", f"z={tc['z']}", f"d={tc['d']}"]
     result = subprocess.run(acvp_call, encoding="utf-8", capture_output=True)
     if result.returncode != 0:
         print("FAIL!")
@@ -81,17 +100,18 @@ def run_keyGen_test(tg, tc):
         exit(1)
     # Extract results and compare to expected data
     for l in result.stdout.splitlines():
-        (k,v) = l.split("=")
+        (k, v) = l.split("=")
         if v != tc[k]:
             print("FAIL!")
             print(f"Mismatching result for {k}: expected {tc[k]}, got {v}")
             exit(1)
     print("OK")
 
+
 for tg in acvp_encapDecap_data["testGroups"]:
     for tc in tg["tests"]:
-        run_encapDecap_test(tg,tc)
+        run_encapDecap_test(tg, tc)
 
 for tg in acvp_keygen_data["testGroups"]:
     for tc in tg["tests"]:
-        run_keyGen_test(tg,tc)
+        run_keyGen_test(tg, tc)
