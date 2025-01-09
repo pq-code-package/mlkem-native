60de445f359770aded324b170ccce3760cd2f47b
Matthias J. Kannwischer <matthias@kannwischer.eu>
2025-01-09 16:26:07 +0800

    AVX2: Try 4xKeccakx4 for 1xKeccak
    Signed-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>

--- mlkem/fips202/native/x86_64/src/xkcp_impl.h
+++ mlkem/fips202/native/x86_64/src/xkcp_impl.h
@@ -10,6 +10,7 @@
 #else
 #define MLKEM_NATIVE_FIPS202_PROFILE_IMPL_H
 
+#include <string.h>
 #include "KeccakP-1600-times4-SnP.h"
 
 #define MLKEM_USE_FIPS202_X4_NATIVE
@@ -17,5 +18,15 @@ static INLINE void keccak_f1600_x4_native(uint64_t *state)
 {
   KeccakP1600times4_PermuteAll_24rounds(state);
 }
+
+#define MLKEM_USE_FIPS202_X1_NATIVE
+static INLINE void keccak_f1600_x1_native(uint64_t *state)
+{
+  uint64_t s[4*25];
+  memcpy(s, state, sizeof(uint64_t)*25);
+  KeccakP1600times4_PermuteAll_24rounds(s);
+  memcpy(state, s, sizeof(uint64_t)*25);
+}
+
 
 #endif /* MLKEM_NATIVE_FIPS202_PROFILE_IMPL_H */
