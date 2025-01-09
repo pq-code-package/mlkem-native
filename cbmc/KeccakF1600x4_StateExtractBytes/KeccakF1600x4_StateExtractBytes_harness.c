6435d1f9e20128dd3da6d16f8185e30462e39041
Matthias J. Kannwischer <matthias@kannwischer.eu>
2025-01-09 14:31:56 +0800

    CBMC: Prove KeccakF1600x4_StateExtractBytes
    Signed-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>

---
+++ cbmc/KeccakF1600x4_StateExtractBytes/KeccakF1600x4_StateExtractBytes_harness.c
@@ -0,0 +1,15 @@
+// Copyright (c) 2024 The mlkem-native project authors
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
+#include <keccakf1600.h>
+
+void harness(void)
+{
+  uint64_t *state;
+  unsigned char *data0, data1, data2, data3;
+  unsigned int offset;
+  unsigned int length;
+  KeccakF1600x4_StateExtractBytes(state, data0, data1, data2, data3, offset,
+                                  length);
+}
