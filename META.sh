0acc335a550751d93edba2f9f1eeeaa2fc4e1824
Hanno Becker <beckphan@amazon.co.uk>
2025-01-22 06:28:42 +0000

    META: Switch back to YAML instead of JSON
    Originally, we used META.yml specify some mlkem-native metadata.
    This was automatically processed in the `tests` script, which
    introduced a dependency on pyyaml and required users to setup a
    virtual environment to merely run basic tests. META.yml was thus
    converted to META.json which could be parsed in Python without
    additional package dependencies.
    
    Now, `tests` no longer parses `META.json`, but instead relies on
    a simple bash script `META.sh` to read the metadata; that script,
    in turn, relies on basic grep and, if present, jq, for the parsing
    of the META.json.
    
    This commit reverts the metadata format back to YML and adjusts
    META.sh to use yq instead of jq, if present.
    
    Signed-off-by: Hanno Becker <beckphan@amazon.co.uk>

--- META.sh
+++ META.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # SPDX-License-Identifier: CC-BY-4.0
 
-# Helper script to query META.json
+# Helper script to query META.yml
 #
 # Arguments
 # - Scheme to query: ML-KEM-512, ML-KEM-768, ML-KEM-1024
@@ -10,20 +10,20 @@
 # Optional:
 # - Value to compare against
 
-META=META.json
+META=META.yml
 
 # Manual extraction of metadata with basic cmd line tools
 VAL=$(cat $META |
-  grep "name\|\"$2\"" |
+  grep "name\|$2" |
   grep $1 -A 1 |
   grep $2 |
-  cut -d ":" -f 2 |
-  tr -d '", ')
+  cut -d ":" -f 2 | tr -d ' ')
 
-# More robust extraction using jq
-if (which jq 2>&1 >/dev/null); then
+# More robust extraction using yq
+if (which yq 2>&1 >/dev/null); then
   QUERY=".implementations | .[] | select(.name==\"$1\") | .\"$2\""
-  VAL_JQ=$(cat $META | jq "$QUERY" -r)
+  echo "cat $META | yq "$QUERY" -r"
+  VAL_JQ=$(cat $META | yq "$QUERY" -r)
 
   if [[ $VAL_JQ != $VAL ]]; then
     echo "ERROR parsing metadata file $META"
