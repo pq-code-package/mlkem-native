44d479e28cda9c51233f693692c7abb37446ee02
Hanno Becker <beckphan@amazon.co.uk>
2025-02-03 15:02:47 +0000

    wip

--- mlkem/native/api.h
+++ mlkem/native/api.h
@@ -211,13 +211,15 @@ __contract__(
 
 #if defined(MLKEM_USE_NATIVE_POLYVEC_BASEMUL_ACC_MONTGOMERY_CACHED)
 /*************************************************
- * Name:        poly_mulcache_compute_native
+ * Name:        poly_mulcache_compute_k2_native
+ *              poly_mulcache_compute_k3_native
+ *              poly_mulcache_compute_k4_native
  *
- * Description: Compute multiplication of polynomials in NTT domain.
+ * Description: Compute scalar product of polynomial vectors in NTT domain.
  *
  * Arguments:   INPUT:
  *              - a: First polynomial operand.
- *                 This must be in NTT domain and inin bitreversed order, or of
+ *                 This must be in NTT domain and in bitreversed order, or of
  *                 a custom order if MLKEM_USE_NATIVE_NTT_CUSTOM_ORDER is set.
  *                 See the documentation of MLKEM_USE_NATIVE_NTT_CUSTOM_ORDER
  *                 for more information.
@@ -228,24 +230,70 @@ __contract__(
  *              - r: Result of the base multiplication. This is again
  *                   in NTT domain, and of the same order as a and b.
  **************************************************/
-static INLINE void polyvec_basemul_acc_montgomery_cached_native(
-    int16_t r[MLKEM_N], const int16_t a[MLKEM_K * MLKEM_N],
-    const int16_t b[MLKEM_K * MLKEM_N],
-    const int16_t b_cache[MLKEM_K * (MLKEM_N / 2)])
+static INLINE void polyvec_basemul_acc_montgomery_cached_k2_native(
+    int16_t r[MLKEM_N], const int16_t a[2 * MLKEM_N],
+    const int16_t b[2 * MLKEM_N],
+    const int16_t b_cache[2 * (MLKEM_N / 2)])
 __contract__(
   requires(memory_no_alias(r, sizeof(int16_t) * MLKEM_N))
-  requires(memory_no_alias(a, sizeof(int16_t) * MLKEM_K * MLKEM_N))
-  requires(memory_no_alias(b, sizeof(int16_t) * MLKEM_K * MLKEM_N))
-  requires(memory_no_alias(b_cache, sizeof(int16_t) * MLKEM_K * (MLKEM_N / 2)))
+  requires(memory_no_alias(a, sizeof(int16_t) * 2 * MLKEM_N))
+  requires(memory_no_alias(b, sizeof(int16_t) * 2 * MLKEM_N))
+  requires(memory_no_alias(b_cache, sizeof(int16_t) * 2 * (MLKEM_N / 2)))
   /* Because of https://github.com/diffblue/cbmc/issues/8570, we can't
    * just use a single flattened array_bound(...) here.
    *
    * Once fixed, change to:
    * ```
-   * requires(array_bound(a, 0, MLKEM_K * MLKEM_N, 0, UINT12_LIMIT))
+   * requires(array_bound(a, 0, 2 * MLKEM_N, 0, UINT12_LIMIT))
    * ```
    */
-  requires(forall(kN, 0, MLKEM_K,					  \
+  requires(forall(kN, 0, 2,					  \
+              array_bound(&((int16_t(*)[MLKEM_N])(a))[kN][0], 0, MLKEM_N, \
+			  0, UINT12_LIMIT)))
+  assigns(memory_slice(r, sizeof(int16_t) * MLKEM_N))
+);
+
+static INLINE void polyvec_basemul_acc_montgomery_cached_k3_native(
+    int16_t r[MLKEM_N], const int16_t a[3 * MLKEM_N],
+    const int16_t b[3 * MLKEM_N],
+    const int16_t b_cache[3 * (MLKEM_N / 2)])
+__contract__(
+  requires(memory_no_alias(r, sizeof(int16_t) * MLKEM_N))
+  requires(memory_no_alias(a, sizeof(int16_t) * 3 * MLKEM_N))
+  requires(memory_no_alias(b, sizeof(int16_t) * 3 * MLKEM_N))
+  requires(memory_no_alias(b_cache, sizeof(int16_t) * 3 * (MLKEM_N / 2)))
+  /* Because of https://github.com/diffblue/cbmc/issues/8570, we can't
+   * just use a single flattened array_bound(...) here.
+   *
+   * Once fixed, change to:
+   * ```
+   * requires(array_bound(a, 0, 3 * MLKEM_N, 0, UINT12_LIMIT))
+   * ```
+   */
+  requires(forall(kN, 0, 3,					  \
+              array_bound(&((int16_t(*)[MLKEM_N])(a))[kN][0], 0, MLKEM_N, \
+			  0, UINT12_LIMIT)))
+  assigns(memory_slice(r, sizeof(int16_t) * MLKEM_N))
+);
+
+static INLINE void polyvec_basemul_acc_montgomery_cached_k4_native(
+    int16_t r[MLKEM_N], const int16_t a[4 * MLKEM_N],
+    const int16_t b[4 * MLKEM_N],
+    const int16_t b_cache[4 * (MLKEM_N / 2)])
+__contract__(
+  requires(memory_no_alias(r, sizeof(int16_t) * MLKEM_N))
+  requires(memory_no_alias(a, sizeof(int16_t) * 4 * MLKEM_N))
+  requires(memory_no_alias(b, sizeof(int16_t) * 4 * MLKEM_N))
+  requires(memory_no_alias(b_cache, sizeof(int16_t) * 4 * (MLKEM_N / 2)))
+  /* Because of https://github.com/diffblue/cbmc/issues/8570, we can't
+   * just use a single flattened array_bound(...) here.
+   *
+   * Once fixed, change to:
+   * ```
+   * requires(array_bound(a, 0, 4 * MLKEM_N, 0, UINT12_LIMIT))
+   * ```
+   */
+  requires(forall(kN, 0, 4,					  \
               array_bound(&((int16_t(*)[MLKEM_N])(a))[kN][0], 0, MLKEM_N, \
 			  0, UINT12_LIMIT)))
   assigns(memory_slice(r, sizeof(int16_t) * MLKEM_N))
