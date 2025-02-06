9a6b959cc764e53e139a07493c35b063c436bc32
Hanno Becker <beckphan@amazon.co.uk>
2025-02-06 05:01:53 +0000

    x86_64 backend: Remove double header guard and unused macro
    Signed-off-by: Hanno Becker <beckphan@amazon.co.uk>

--- dev/x86_64/src/align.h
+++ dev/x86_64/src/align.h
@@ -10,18 +10,8 @@
  * https://github.com/pq-crystals/kyber/blob/main/avx2/align.h
  */
 
-#ifndef ALIGN_H
-#define ALIGN_H
-
 #include <immintrin.h>
 #include <stdint.h>
-
-#define ALIGNED_UINT8(N)        \
-  union                         \
-  {                             \
-    uint8_t coeffs[N];          \
-    __m256i vec[(N + 31) / 32]; \
-  }
 
 #define ALIGNED_INT16(N)        \
   union                         \
@@ -29,7 +19,5 @@
     int16_t coeffs[N];          \
     __m256i vec[(N + 15) / 16]; \
   }
-
-#endif
 
 #endif /* MLK_DEV_X86_64_SRC_ALIGN_H */
