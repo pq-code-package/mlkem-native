bfe6007422da98f777c8b99982e1aaf43782d29d
Hanno Becker <beckphan@amazon.co.uk>
2025-03-05 09:43:31 +0000

    AArch64: Add HOL-Light proof for triple hybrid Keccak-f1600-x4
    This commit adds a proof of functional correctness for the highly
    complex triple-hybrid implementation of Keccak-F1600-x4. The
    implementation uses both scalar and Neon execution units, and
    distributes Neon instructions across 'classical' v8-A and
    v8.4-A+SHA3 instructions to ensure fastest execution on Arm
    cores with heterogeneous Neon execution units.
    
    The proof is contributed by John Harrison and utilizes the
    s2n-bignum verification infrastructure.
    
    Signed-off-by: Hanno Becker <beckphan@amazon.co.uk>

--- nix/s2n_bignum/default.nix
+++ nix/s2n_bignum/default.nix
@@ -2,12 +2,12 @@
 { stdenv, fetchFromGitHub, writeText, ... }:
 stdenv.mkDerivation rec {
   pname = "s2n_bignum";
-  version = "a7e47d3093b62625172f0e0bf4168c838ce1d955";
+  version = "7c018f70667310c96ac5d6c27468104117bd51c0";
   src = fetchFromGitHub {
     owner = "jargh";
     repo = "s2n-bignum-dev";
     rev = "${version}";
-    hash = "sha256-8pGeDR9U80wOP/PIPTJBITUt0JZu13EGwbYOD5INBa0";
+    hash = "sha256-/Iyz2mnxGSbmte9lT1JbY4WcBmWWrc7tUpZ0HfAUD+4";
   };
   setupHook = writeText "setup-hook.sh" ''
     export S2N_BIGNUM_DIR="$1"
