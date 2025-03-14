b10bd1a60090c0d8eba7778181e10ea66d8c4c61
Hanno Becker <beckphan@amazon.co.uk>
2025-03-14 04:38:02 +0000

    Update CBMC to version 6.5.0
    Signed-off-by: Hanno Becker <beckphan@amazon.co.uk>

--- nix/cbmc/default.nix
+++ nix/cbmc/default.nix
@@ -14,12 +14,12 @@ buildEnv {
   paths =
     builtins.attrValues {
       cbmc = cbmc.overrideAttrs (old: rec {
-        version = "e033ba1f10121e5a913991ab6fc672d34a115ec1"; # remember to adjust this in ../flake.nix too
+        version = "6.5.0";
         src = fetchFromGitHub {
           owner = "diffblue";
           repo = "cbmc";
-          rev = "e033ba1f10121e5a913991ab6fc672d34a115ec1";
-          hash = "sha256-m2LhpldzBSYP9caQ2RJBl++Cx5Fx97y8E2P2SKcsmMM=";
+          rev = "32143ddf8ae93e6bd0f52189de509662348c2373";
+          hash = "sha256-uciXv4dqESQrbNz+bQFpJcqoeWO8ZaRYjxoSUBOjlNI=";
         };
       });
       litani = callPackage ./litani.nix { }; # 1.29.0
