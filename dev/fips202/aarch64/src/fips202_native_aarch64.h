4b50aac2519e7edaaee8704e24b225bbad5d6459
Matthias J. Kannwischer <matthias@kannwischer.eu>
2025-05-19 13:26:51 +0800

    CBMC: Prove keccak_f1600_x4_native based on keccak_f1600_x4_scalar_v8a_asm_hybrid
    Signed-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>

--- dev/fips202/aarch64/src/fips202_native_aarch64.h
+++ dev/fips202/aarch64/src/fips202_native_aarch64.h
@@ -41,7 +41,12 @@ __contract__(
 #define mlk_keccak_f1600_x4_scalar_v8a_asm_hybrid \
   MLK_NAMESPACE(keccak_f1600_x4_scalar_v8a_asm_hybrid)
 void mlk_keccak_f1600_x4_scalar_v8a_asm_hybrid(uint64_t *state,
-                                               uint64_t const *rc);
+                                               uint64_t const *rc)
+__contract__(
+  requires(memory_no_alias(state, sizeof(uint64_t) * 25 * 4))
+  requires(rc == mlk_keccakf1600_round_constants)
+  assigns(memory_slice(state, sizeof(uint64_t) * 25 * 4))
+);
 
 #define mlk_keccak_f1600_x4_scalar_v84a_asm_hybrid \
   MLK_NAMESPACE(keccak_f1600_x4_scalar_v84a_asm_hybrid)
