a768d7ccb7d042f75062626e2bcd0bc1a0954012
Matthias J. Kannwischer <matthias@kannwischer.eu>
2025-05-19 13:08:17 +0800

    CBMC: Prove AArch64 keccak_f1600_x1_native based on keccak_f1600_x1_scalar_asm
    Signed-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>

--- mlkem/fips202/native/aarch64/src/fips202_native_aarch64.h
+++ mlkem/fips202/native/aarch64/src/fips202_native_aarch64.h
@@ -6,10 +6,21 @@
 #define MLK_FIPS202_NATIVE_AARCH64_SRC_FIPS202_NATIVE_AARCH64_H
 
 #include <stdint.h>
+#include "../../../../cbmc.h"
 #include "../../../../common.h"
 
+
+#define mlk_keccakf1600_round_constants \
+  MLK_NAMESPACE(keccakf1600_round_constants)
+extern const uint64_t mlk_keccakf1600_round_constants[];
+
 #define mlk_keccak_f1600_x1_scalar_asm MLK_NAMESPACE(keccak_f1600_x1_scalar_asm)
-void mlk_keccak_f1600_x1_scalar_asm(uint64_t *state, uint64_t const *rc);
+void mlk_keccak_f1600_x1_scalar_asm(uint64_t *state, uint64_t const *rc)
+__contract__(
+  requires(memory_no_alias(state, sizeof(uint64_t) * 25 * 1))
+  requires(rc == mlk_keccakf1600_round_constants)
+  assigns(memory_slice(state, sizeof(uint64_t) * 25 * 1))
+);
 
 #define mlk_keccak_f1600_x1_v84a_asm MLK_NAMESPACE(keccak_f1600_x1_v84a_asm)
 void mlk_keccak_f1600_x1_v84a_asm(uint64_t *state, uint64_t const *rc);
@@ -37,8 +48,5 @@ void mlk_keccak_f1600_x4_scalar_v84a_asm_hybrid(uint64_t *state,
 void mlk_keccak_f1600_x4_scalar_v8a_v84a_hybrid_asm(uint64_t *state,
                                                     uint64_t const *rc);
 
-#define mlk_keccakf1600_round_constants \
-  MLK_NAMESPACE(keccakf1600_round_constants)
-extern const uint64_t mlk_keccakf1600_round_constants[];
 
 #endif /* !MLK_FIPS202_NATIVE_AARCH64_SRC_FIPS202_NATIVE_AARCH64_H */
