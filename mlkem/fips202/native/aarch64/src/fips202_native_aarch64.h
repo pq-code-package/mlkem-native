6b661c6ffffd1f24963c5969f11e20080d3b095d
Matthias J. Kannwischer <matthias@kannwischer.eu>
2025-05-19 13:35:07 +0800

    Rename keccak_f1600_x4_scalar_v8a_asm_hybrid -> keccak_f1600_x4_scalar_v8a_hybrid_asm
    Signed-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>

--- mlkem/fips202/native/aarch64/src/fips202_native_aarch64.h
+++ mlkem/fips202/native/aarch64/src/fips202_native_aarch64.h
@@ -38,9 +38,9 @@ __contract__(
   assigns(memory_slice(state, sizeof(uint64_t) * 25 * 2))
 );
 
-#define mlk_keccak_f1600_x4_scalar_v8a_asm_hybrid \
-  MLK_NAMESPACE(keccak_f1600_x4_scalar_v8a_asm_hybrid)
-void mlk_keccak_f1600_x4_scalar_v8a_asm_hybrid(uint64_t *state,
+#define mlk_keccak_f1600_x4_scalar_v8a_hybrid_asm \
+  MLK_NAMESPACE(keccak_f1600_x4_scalar_v8a_hybrid_asm)
+void mlk_keccak_f1600_x4_scalar_v8a_hybrid_asm(uint64_t *state,
                                                uint64_t const *rc)
 __contract__(
   requires(memory_no_alias(state, sizeof(uint64_t) * 25 * 4))
--- mlkem/fips202/native/aarch64/x4_v8a_scalar.h
+++ mlkem/fips202/native/aarch64/x4_v8a_scalar.h
@@ -15,7 +15,7 @@
 #include "src/fips202_native_aarch64.h"
 static MLK_INLINE void mlk_keccak_f1600_x4_native(uint64_t *state)
 {
-  mlk_keccak_f1600_x4_scalar_v8a_asm_hybrid(state,
+  mlk_keccak_f1600_x4_scalar_v8a_hybrid_asm(state,
                                             mlk_keccakf1600_round_constants);
 }
 #endif /* !__ASSEMBLER__ */
