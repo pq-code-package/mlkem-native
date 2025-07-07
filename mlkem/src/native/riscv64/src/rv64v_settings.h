4a9b5cb4fac3d1311c58eafbc7174bcf9a719777
Matthias J. Kannwischer <matthias@kannwischer.eu>
2025-07-07 10:10:27 +0800

    RV64: Namespace constants
    Signed-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>

--- mlkem/src/native/riscv64/src/rv64v_settings.h
+++ mlkem/src/native/riscv64/src/rv64v_settings.h
@@ -18,15 +18,15 @@
 /*  Montgomery reduction constants */
 /*  n   = 256; q   = 3329; r   = 2^16 */
 /* check-magic: 3327 == unsigned_mod(-pow(MLKEM_Q,-1,2^16), 2^16) */
-#define MLKEM_QI 3327
+#define MLK_RVV_QI 3327
 
 /* check-magic: 2285 == unsigned_mod(2^16, MLKEM_Q) */
-#define MLK_MONT_R1 2285
+#define MLK_RVV_MONT_R1 2285
 
 /* check-magic: 1353 == pow(2, 32, MLKEM_Q) */
-#define MLK_MONT_R2 1353
+#define MLK_RVV_MONT_R2 1353
 
 /* check-magic: 1441 == pow(2,32-7,MLKEM_Q) */
-#define MLK_MONT_NR 1441
+#define MLK_RVV_MONT_NR 1441
 
 #endif /* !MLK_NATIVE_RISCV64_SRC_RV64V_SETTINGS_H */
