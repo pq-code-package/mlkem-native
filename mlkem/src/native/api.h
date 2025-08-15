d8573f4c4a83541fb76716f782a02cd84617ebee
Rod Chapman <rodchap@amazon.com>
2025-08-15 16:50:50 +0100

    Strengthen postcondition to show polynomials are unchanged in the failure case.
    For the following functions, all of which take and return a single
    in-place parameter:
      ntt_native()
      intt_native()
      poly_reduce_native()
      poly_tomont_native()
    
    Signed-off-by: Rod Chapman <rodchap@amazon.com>

--- mlkem/src/native/api.h
+++ mlkem/src/native/api.h
@@ -95,6 +95,7 @@ __contract__(
   ensures(return_value == MLK_NATIVE_FUNC_FAIL || return_value == MLK_NATIVE_FUNC_SUCCESS)
   ensures((return_value == MLK_NATIVE_FUNC_SUCCESS) ==> array_abs_bound(p, 0, MLKEM_N, MLK_NTT_BOUND))
   ensures((return_value == MLK_NATIVE_FUNC_FAIL) ==> array_abs_bound(p, 0, MLKEM_N, MLKEM_Q))
+  ensures((return_value == MLK_NATIVE_FUNC_FAIL) ==> forall(k, 0, MLKEM_N, p[k] == old(* (int16_t (*)[MLKEM_N])p)[k]))
 );
 #endif /* MLK_USE_NATIVE_NTT */
 
@@ -161,6 +162,7 @@ __contract__(
   assigns(memory_slice(p, sizeof(int16_t) * MLKEM_N))
   ensures(return_value == MLK_NATIVE_FUNC_FAIL || return_value == MLK_NATIVE_FUNC_SUCCESS)
   ensures((return_value == MLK_NATIVE_FUNC_SUCCESS) ==> array_abs_bound(p, 0, MLKEM_N, MLK_INVNTT_BOUND))
+  ensures((return_value == MLK_NATIVE_FUNC_FAIL) ==> forall(k, 0, MLKEM_N, p[k] == old(* (int16_t (*)[MLKEM_N])p)[k]))
 );
 #endif /* MLK_USE_NATIVE_INTT */
 
@@ -178,6 +180,7 @@ __contract__(
   assigns(memory_slice(p, sizeof(int16_t) * MLKEM_N))
   ensures(return_value == MLK_NATIVE_FUNC_FAIL || return_value == MLK_NATIVE_FUNC_SUCCESS)
   ensures((return_value == MLK_NATIVE_FUNC_SUCCESS) ==> array_bound(p, 0, MLKEM_N, 0, MLKEM_Q))
+  ensures((return_value == MLK_NATIVE_FUNC_FAIL) ==> forall(k, 0, MLKEM_N, p[k] == old(* (int16_t (*)[MLKEM_N])p)[k]))
 );
 #endif /* MLK_USE_NATIVE_POLY_REDUCE */
 
@@ -196,6 +199,7 @@ __contract__(
   assigns(memory_slice(p, sizeof(int16_t) * MLKEM_N))
   ensures(return_value == MLK_NATIVE_FUNC_FAIL || return_value == MLK_NATIVE_FUNC_SUCCESS)
   ensures((return_value == MLK_NATIVE_FUNC_SUCCESS) ==> array_abs_bound(p, 0, MLKEM_N, MLKEM_Q))
+  ensures((return_value == MLK_NATIVE_FUNC_FAIL) ==> forall(k, 0, MLKEM_N, p[k] == old(* (int16_t (*)[MLKEM_N])p)[k]))
 );
 #endif /* MLK_USE_NATIVE_POLY_TOMONT */
 
