b58b0c49f82dfd966f33bb45ef5c74f159f26f39
Rod Chapman <rodchap@amazon.com>
2025-08-15 12:45:08 +0100

    Add CBMC proof for is_native_capable()
    Signed-off-by: Rod Chapman <rodchap@amazon.com>

---
+++ proofs/cbmc/is_native_capable/is_native_capable_harness.c
@@ -0,0 +1,11 @@
+// Copyright (c) The mlkem-native project authors
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
+#include "native_capability.h"
+
+void harness(void)
+{
+  int t;
+  t = mlk_is_native_capable();
+}
