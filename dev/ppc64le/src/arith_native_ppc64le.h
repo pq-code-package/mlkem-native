0a81791f7a6a771c5602aecf5898280543b87590
Danny Tsen <dtsen@us.ibm.com>
2025-09-08 11:53:55 -0400

    Added optimized ppc64le support functions for ML-KEM.
    The supported native functions include:
    1. MLK_USE_NATIVE_NTT (ntt_ppc.S)
    2. MLK_USE_NATIVE_INTT (intt_ppc.S)
    3. MLK_USE_NATIVE_POLY_REDUCE (reduce.S)
    4. MLK_USE_NATIVE_POLY_TOMONT (poly_tomont.S)
    
    And other interface functions and headers.
    
    Signed-off-by: Danny Tsen <dtsen@us.ibm.com>

---
+++ dev/ppc64le/src/arith_native_ppc64le.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2024-2025 The mlkem-native project authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef MLK_NATIVE_PPC64LE_SRC_ARITH_NATIVE_PPC64LE_H
+#define MLK_NATIVE_PPC64LE_SRC_ARITH_NATIVE_PPC64LE_H
+
+#include <stdint.h>
+#include "../../../common.h"
+
+#define mlk_ntt_ppc MLK_NAMESPACE(ntt_ppc)
+void mlk_ntt_ppc(int16_t *);
+
+#define mlk_intt_ppc MLK_NAMESPACE(intt_ppc)
+void mlk_intt_ppc(int16_t *);
+
+#define mlk_reduce_ppc MLK_NAMESPACE(reduce_ppc)
+void mlk_reduce_ppc(int16_t *r);
+
+#define mlk_poly_tomont_ppc MLK_NAMESPACE(poly_tomont_ppc)
+void mlk_poly_tomont_ppc(int16_t *);
+
+#endif /* MLK_NATIVE_PPC64LE_SRC_ARITH_NATIVE_PPC64LE_H */
