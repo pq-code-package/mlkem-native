48fddba633764119f03201d2b08e6b662c4429d2
Rod Chapman <rodchap@amazon.com>
2025-11-05 21:34:45 +0000

    Update NIX config for Z4 4.15.3
    Signed-off-by: Rod Chapman <rodchap@amazon.com>

--- nix/cbmc/default.nix
+++ nix/cbmc/default.nix
@@ -30,34 +30,13 @@ buildEnv {
       litani = callPackage ./litani.nix { }; # 1.29.0
       cbmc-viewer = callPackage ./cbmc-viewer.nix { }; # 3.11
       z3 = z3.overrideAttrs (old: rec {
-        version = "4.12.6";
+        version = "4.15.3";
         src = fetchFromGitHub {
           owner = "Z3Prover";
           repo = "z3";
-          rev = "z3-4.12.6";
-          hash = "sha256-X4wfPWVSswENV0zXJp/5u9SQwGJWocLKJ/CNv57Bt+E=";
+          rev = "z3-4.15.3";
+          hash = "sha256-Lw037Z0t0ySxkgMXkbjNW5CB4QQLRrrSEBsLJqiomZ4=";
         };
-
-        static-matrix-patch = fetchpatch {
-          name = "gcc-15-fixes.patch";
-          url = "https://github.com/Z3Prover/z3/commit/2ce89e5f491fa817d02d8fdce8c62798beab258b.patch";
-          hash = "sha256-UvrUL27o/w1/sH/hO7bmvVupg3vbSjEqoIpoZh2BOhg=";
-          includes = [ "src/math/lp/static_matrix.h" ];
-        };
-
-        static-matrix-def-patch = fetchpatch {
-          # clang / gcc fixes. fixes typos in some member names
-          name = "gcc-15-fixes.patch";
-          url = "https://github.com/Z3Prover/z3/commit/2ce89e5f491fa817d02d8fdce8c62798beab258b.patch";
-          includes = [ "src/math/lp/static_matrix_def.h" ];
-          hash = "sha256-rEH+UzylzyhBdtx65uf8QYj5xwuXOyG6bV/4jgKkXGo=";
-        };
-
-        patches = [
-          ./z3-lower-bound-typo.patch
-          static-matrix-def-patch
-          static-matrix-patch
-        ];
       });
 
       inherit
