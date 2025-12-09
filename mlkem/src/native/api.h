398be60a83ba63af32e7201f62e18216667c5ee1
Matthias J. Kannwischer <matthias@kannwischer.eu>
2025-12-09 13:27:09 +0800

    Minor clean-up of mlk_rej_uniform_native contract
    Fixes single instance of -1 that should be MLK_NATIVE_FUNC_FALLBACK.
    
    Signed-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>
    Signed-off-by: Hanno Becker <beckphan@amazon.co.uk>

--- mlkem/src/native/api.h
+++ mlkem/src/native/api.h
@@ -413,8 +413,10 @@ __contract__(
   requires(memory_no_alias(r, sizeof(int16_t) * len))
   requires(memory_no_alias(buf, buflen))
   assigns(memory_slice(r, sizeof(int16_t) * len))
-  ensures(return_value == MLK_NATIVE_FUNC_FALLBACK || (0 <= return_value && return_value <= len))
-  ensures(return_value != -1 ==> array_bound(r, 0, (unsigned) return_value, 0, MLKEM_Q))
+  ensures(return_value != MLK_NATIVE_FUNC_FALLBACK
+	      ==> (0 <= return_value && return_value <= len))
+  ensures(return_value != MLK_NATIVE_FUNC_FALLBACK
+	      ==> array_bound(r, 0, (unsigned) return_value, 0, MLKEM_Q))
 );
 #endif /* MLK_USE_NATIVE_REJ_UNIFORM */
 
