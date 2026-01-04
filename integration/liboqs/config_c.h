ad5478cdf49960bce9ea24e70a9e7e878b5f3de1
Andreas Hatziiliou <andreas.hatziiliou@savoirfairelinux.com>
2026-01-04 18:56:51 -0500

    API: add failure mode support for randombytes()
    Change randombytes() to return int (0 on success, non-zero on failure)
    instead of void, allowing callers to detect and handle RNG failures.
    
    This commit:
    
    * Updates function signatures.
    * All call sites to check return values.
    * Changes test files to use CHECK macro.
    * Adds documentation of the new failure mode to sign.h and
      mlkem_native.h
    * Adds a new error code MLK_ERR_RNG_FAIL.
    * Declares src/randombytes with MLK_MUST_CHECK_RETURN_VALUE.
    
    Signed-off-by: Andreas Hatziiliou <andreas.hatziiliou@savoirfairelinux.com>

--- integration/liboqs/config_c.h
+++ integration/liboqs/config_c.h
@@ -145,7 +145,7 @@
  *              consumer.
  *
  *              If this option is not set, mlkem-native expects a function
- *              void randombytes(uint8_t *out, size_t outlen).
+ *              int randombytes(uint8_t *out, size_t outlen).
  *
  *              Set this option and define `mlk_randombytes` if you want to
  *              use a custom method to sample randombytes with a different name
@@ -157,9 +157,10 @@
 #include <oqs/rand.h>
 #include <stdint.h>
 #include "../../mlkem/src/sys.h"
-static MLK_INLINE void mlk_randombytes(uint8_t *ptr, size_t len)
+static MLK_INLINE int mlk_randombytes(uint8_t *ptr, size_t len)
 {
   OQS_randombytes(ptr, len);
+  return 0;
 }
 #endif /* !__ASSEMBLER__ */
 
