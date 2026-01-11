6876ac64fe75c84ae9b9975f6a9f14277c147448
Matthias J. Kannwischer <matthias@kannwischer.eu>
2026-01-11 10:50:05 +0800

    Examples: Set MLK_CONFIG_NO_SUPERCOP for multilevel builds
    Multilevel builds require MLK_CONFIG_NO_SUPERCOP since the SUPERCOP
    naming convention (crypto_kem_xxx) does not disambiguate between
    parameter sets.
    
    Add MLK_CONFIG_NO_SUPERCOP to all multilevel build configs and
    propagate it to the API layer in the legacy config path.
    
    This also fixes problem with mlkem_native.h: On multiple inclusions of
    mlkem_native.h without NO_SUPERCOP set, mlkem_native.h would not undef the
    API_CONFIG macros in the end of the file causing the second inclusion of
    mlkem_native.h to set the LEGACY_CONFIG option.
    
     - Ported from https://github.com/pq-code-package/mldsa-native/pull/869
    
    Signed-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>

--- examples/monolithic_build_multilevel/mlkem_native/mlkem_native_config.h
+++ examples/monolithic_build_multilevel/mlkem_native/mlkem_native_config.h
@@ -29,6 +29,7 @@
  *
  * This configuration differs from the default mlkem/mlkem_native_config.h in
  * the following places:
+ *   - MLK_CONFIG_NO_SUPERCOP
  *   - MLK_CONFIG_MULTILEVEL_BUILD
  *   - MLK_CONFIG_NAMESPACE_PREFIX
  *   - MLK_CONFIG_INTERNAL_API_QUALIFIER
@@ -156,7 +157,7 @@
  *              naming does not disambiguate between the parameter sets.
  *
  *****************************************************************************/
-/* #define MLK_CONFIG_NO_SUPERCOP */
+#define MLK_CONFIG_NO_SUPERCOP
 
 /******************************************************************************
  * Name:        MLK_CONFIG_CONSTANTS_ONLY
