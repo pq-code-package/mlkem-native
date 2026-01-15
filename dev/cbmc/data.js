58d00f690e9a43a93f2b627edff27a6896cd3ade
github-action-benchmark <github@users.noreply.github.com>
2026-01-15 07:54:30 +0000

    add CBMC Runtime (ML-KEM-512) (customSmallerIsBetter) benchmark result for 45915e14a00bd6d04bec0f3df1d8fac6e9e47cb9

--- dev/cbmc/data.js
+++ dev/cbmc/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1768463669692,
+  "lastUpdate": 1768463670655,
   "repoUrl": "https://github.com/pq-code-package/mlkem-native",
   "entries": {
     "CBMC Runtime (ML-KEM-1024)": [
@@ -66,6 +66,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "poly_tomont_native_aarch64",
             "value": 3,
+            "unit": "seconds"
+          }
+        ]
+      }
+    ],
+    "CBMC Runtime (ML-KEM-512)": [
+      {
+        "commit": {
+          "author": {
+            "name": "pq-code-package",
+            "username": "pq-code-package"
+          },
+          "committer": {
+            "name": "pq-code-package",
+            "username": "pq-code-package"
+          },
+          "id": "45915e14a00bd6d04bec0f3df1d8fac6e9e47cb9",
+          "message": "CI: Comment on PRs if CBMC proofs fail",
+          "timestamp": "2026-01-13T21:45:38Z",
+          "url": "https://github.com/pq-code-package/mlkem-native/pull/1478/commits/45915e14a00bd6d04bec0f3df1d8fac6e9e47cb9"
+        },
+        "date": 1768463588674,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "mlk_poly_tomont",
+            "value": 999999,
+            "unit": "seconds",
+            "extra": "Failed in 2 seconds"
+          },
+          {
+            "name": "mlk_poly_tomont_native",
+            "value": 999999,
+            "unit": "seconds",
+            "extra": "Failed in 2 seconds"
+          },
+          {
+            "name": "mlk_poly_tobytes",
+            "value": 3,
+            "unit": "seconds"
+          },
+          {
+            "name": "mlk_poly_tobytes_c",
+            "value": 2,
+            "unit": "seconds"
+          },
+          {
+            "name": "mlk_poly_tobytes_native",
+            "value": 2,
+            "unit": "seconds"
+          },
+          {
+            "name": "mlk_poly_tomont_c",
+            "value": 2,
+            "unit": "seconds"
+          },
+          {
+            "name": "mlk_poly_tomsg",
+            "value": 1,
+            "unit": "seconds"
+          },
+          {
+            "name": "poly_tobytes_native_aarch64",
+            "value": 2,
+            "unit": "seconds"
+          },
+          {
+            "name": "poly_tomont_native_aarch64",
+            "value": 1,
             "unit": "seconds"
           }
         ]
