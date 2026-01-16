64038cdd80e3aff02117afdf0846ba04e8a906ad
github-action-benchmark <github@users.noreply.github.com>
2026-01-16 04:56:47 +0000

    add CBMC Runtime (ML-KEM-768) (customSmallerIsBetter) benchmark result for 39ee7ff841a4e201a4cd954239df229ab4298291

--- dev/cbmc/data.js
+++ dev/cbmc/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1768539297962,
+  "lastUpdate": 1768539407611,
   "repoUrl": "https://github.com/pq-code-package/mlkem-native",
   "entries": {
     "CBMC Runtime (ML-KEM-1024)": [
@@ -795,6 +795,83 @@ window.BENCHMARK_DATA = {
           {
             "name": "polyvec_basemul_acc_montgomery_cached_native",
             "value": 59,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "pq-code-package",
+            "username": "pq-code-package"
+          },
+          "committer": {
+            "name": "pq-code-package",
+            "username": "pq-code-package"
+          },
+          "id": "39ee7ff841a4e201a4cd954239df229ab4298291",
+          "message": "CI: Comment on PRs if CBMC proofs fail",
+          "timestamp": "2026-01-13T21:45:38Z",
+          "url": "https://github.com/pq-code-package/mlkem-native/pull/1478/commits/39ee7ff841a4e201a4cd954239df229ab4298291"
+        },
+        "date": 1768539325215,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "mlk_poly_tomont",
+            "value": 999999,
+            "unit": "seconds",
+            "extra": "Failed in 3 seconds"
+          },
+          {
+            "name": "mlk_poly_tomont_native",
+            "value": 999999,
+            "unit": "seconds",
+            "extra": "Failed in 2 seconds"
+          },
+          {
+            "name": "keccakf1600x4_permute_native_x4",
+            "value": 20,
+            "unit": "seconds"
+          },
+          {
+            "name": "mlk_indcpa_enc",
+            "value": 377,
+            "unit": "seconds"
+          },
+          {
+            "name": "mlk_indcpa_keypair_derand",
+            "value": 546,
+            "unit": "seconds"
+          },
+          {
+            "name": "mlk_keccak_squeezeblocks_x4",
+            "value": 138,
+            "unit": "seconds"
+          },
+          {
+            "name": "mlk_poly_rej_uniform",
+            "value": 33,
+            "unit": "seconds"
+          },
+          {
+            "name": "mlk_polyvec_basemul_acc_montgomery_cached_c",
+            "value": 71,
+            "unit": "seconds"
+          },
+          {
+            "name": "mlk_rej_uniform_c",
+            "value": 67,
+            "unit": "seconds"
+          },
+          {
+            "name": "poly_ntt_native",
+            "value": 21,
+            "unit": "seconds"
+          },
+          {
+            "name": "polyvec_basemul_acc_montgomery_cached_native",
+            "value": 48,
             "unit": "seconds"
           }
         ]
