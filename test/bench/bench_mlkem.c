8a145b9cbab8d856e1bb070af114bf9c7764d537
Matthias J. Kannwischer <matthias@kannwischer.eu>
2026-01-21 10:17:54 +0800

    Rename benchmark parameters to MLK_BENCHMARK_xxx
    Make the benchmark parameters (NWARMUP, NITERATIONS, NTESTS)
    configurable via CFLAGS by wrapping them in #ifndef guards and
    renaming to MLK_BENCHMARK_NWARMUP, MLK_BENCHMARK_NITERATIONS,
    and MLK_BENCHMARK_NTESTS.
    
    Signed-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>

--- test/bench/bench_mlkem.c
+++ test/bench/bench_mlkem.c
@@ -13,9 +13,15 @@
 
 #include "../../mlkem/mlkem_native.h"
 
-#define NWARMUP 50
-#define NITERATIONS 300
-#define NTESTS 500
+#ifndef MLK_BENCHMARK_NWARMUP
+#define MLK_BENCHMARK_NWARMUP 50
+#endif
+#ifndef MLK_BENCHMARK_NITERATIONS
+#define MLK_BENCHMARK_NITERATIONS 300
+#endif
+#ifndef MLK_BENCHMARK_NTESTS
+#define MLK_BENCHMARK_NTESTS 500
+#endif
 
 #define CHECK(x)                                              \
   do                                                          \
@@ -34,9 +40,10 @@ static int cmp_uint64_t(const void *a, const void *b)
   return (int)((*((const uint64_t *)a)) - (*((const uint64_t *)b)));
 }
 
-static void print_median(const char *txt, uint64_t cyc[NTESTS])
+static void print_median(const char *txt, uint64_t cyc[MLK_BENCHMARK_NTESTS])
 {
-  printf("%10s cycles = %" PRIu64 "\n", txt, cyc[NTESTS >> 1] / NITERATIONS);
+  printf("%10s cycles = %" PRIu64 "\n", txt,
+         cyc[MLK_BENCHMARK_NTESTS >> 1] / MLK_BENCHMARK_NITERATIONS);
 }
 
 static int percentiles[] = {1, 10, 20, 30, 40, 50, 60, 70, 80, 90, 99};
@@ -47,18 +54,20 @@ static void print_percentile_legend(void)
   printf("%21s", "percentile");
   for (i = 0; i < sizeof(percentiles) / sizeof(percentiles[0]); i++)
   {
-    printf("%7d", percentiles[i]);
+    printf("%9d", percentiles[i]);
   }
   printf("\n");
 }
 
-static void print_percentiles(const char *txt, uint64_t cyc[NTESTS])
+static void print_percentiles(const char *txt,
+                              uint64_t cyc[MLK_BENCHMARK_NTESTS])
 {
   unsigned i;
   printf("%10s percentiles:", txt);
   for (i = 0; i < sizeof(percentiles) / sizeof(percentiles[0]); i++)
   {
-    printf("%7" PRIu64, (cyc)[NTESTS * percentiles[i] / 100] / NITERATIONS);
+    printf("%9" PRIu64, (cyc)[MLK_BENCHMARK_NTESTS * percentiles[i] / 100] /
+                            MLK_BENCHMARK_NITERATIONS);
   }
   printf("\n");
 }
@@ -71,26 +80,27 @@ static int bench(void)
   uint8_t key_a[CRYPTO_BYTES];
   uint8_t key_b[CRYPTO_BYTES];
   unsigned char kg_rand[2 * CRYPTO_BYTES], enc_rand[CRYPTO_BYTES];
-  uint64_t cycles_kg[NTESTS], cycles_enc[NTESTS], cycles_dec[NTESTS];
+  uint64_t cycles_kg[MLK_BENCHMARK_NTESTS], cycles_enc[MLK_BENCHMARK_NTESTS],
+      cycles_dec[MLK_BENCHMARK_NTESTS];
 
   unsigned i, j;
   uint64_t t0, t1;
 
 
-  for (i = 0; i < NTESTS; i++)
+  for (i = 0; i < MLK_BENCHMARK_NTESTS; i++)
   {
     int ret = 0;
     randombytes(kg_rand, 2 * CRYPTO_BYTES);
     randombytes(enc_rand, CRYPTO_BYTES);
 
     /* Key-pair generation */
-    for (j = 0; j < NWARMUP; j++)
+    for (j = 0; j < MLK_BENCHMARK_NWARMUP; j++)
     {
       ret |= crypto_kem_keypair_derand(pk, sk, kg_rand);
     }
 
     t0 = get_cyclecounter();
-    for (j = 0; j < NITERATIONS; j++)
+    for (j = 0; j < MLK_BENCHMARK_NITERATIONS; j++)
     {
       ret |= crypto_kem_keypair_derand(pk, sk, kg_rand);
     }
@@ -99,12 +109,12 @@ static int bench(void)
 
 
     /* Encapsulation */
-    for (j = 0; j < NWARMUP; j++)
+    for (j = 0; j < MLK_BENCHMARK_NWARMUP; j++)
     {
       ret |= crypto_kem_enc_derand(ct, key_a, pk, enc_rand);
     }
     t0 = get_cyclecounter();
-    for (j = 0; j < NITERATIONS; j++)
+    for (j = 0; j < MLK_BENCHMARK_NITERATIONS; j++)
     {
       ret |= crypto_kem_enc_derand(ct, key_a, pk, enc_rand);
     }
@@ -112,12 +122,12 @@ static int bench(void)
     cycles_enc[i] = t1 - t0;
 
     /* Decapsulation */
-    for (j = 0; j < NWARMUP; j++)
+    for (j = 0; j < MLK_BENCHMARK_NWARMUP; j++)
     {
       ret |= crypto_kem_dec(key_b, ct, sk);
     }
     t0 = get_cyclecounter();
-    for (j = 0; j < NITERATIONS; j++)
+    for (j = 0; j < MLK_BENCHMARK_NITERATIONS; j++)
     {
       ret |= crypto_kem_dec(key_b, ct, sk);
     }
@@ -128,9 +138,9 @@ static int bench(void)
     CHECK(memcmp(key_a, key_b, CRYPTO_BYTES) == 0);
   }
 
-  qsort(cycles_kg, NTESTS, sizeof(uint64_t), cmp_uint64_t);
-  qsort(cycles_enc, NTESTS, sizeof(uint64_t), cmp_uint64_t);
-  qsort(cycles_dec, NTESTS, sizeof(uint64_t), cmp_uint64_t);
+  qsort(cycles_kg, MLK_BENCHMARK_NTESTS, sizeof(uint64_t), cmp_uint64_t);
+  qsort(cycles_enc, MLK_BENCHMARK_NTESTS, sizeof(uint64_t), cmp_uint64_t);
+  qsort(cycles_dec, MLK_BENCHMARK_NTESTS, sizeof(uint64_t), cmp_uint64_t);
 
   print_median("keypair", cycles_kg);
   print_median("encaps", cycles_enc);
