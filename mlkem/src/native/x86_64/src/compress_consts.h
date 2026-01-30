83a654292598d6f490f6d3daba258d7f3e467d6c
Matthias J. Kannwischer <matthias@kannwischer.eu>
2026-01-30 14:10:09 +0800

    x86_64: Convert poly_decompress_d4_avx2 from intrinsics to assembly
    Cherry-picked from https://github.com/pq-code-package/mlkem-native/pull/1499.
    
    Autogen is adjusted the generate the required constants in compress_consts.c.
    
    Signed-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>

---
+++ mlkem/src/native/x86_64/src/compress_consts.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) The mlkem-native project authors
+ * SPDX-License-Identifier: Apache-2.0 OR ISC OR MIT
+ */
+
+/*
+ * WARNING: This file is auto-generated from scripts/autogen
+ *          in the mlkem-native repository.
+ *          Do not modify it directly.
+ */
+
+#ifndef MLK_NATIVE_X86_64_SRC_COMPRESS_CONSTS_H
+#define MLK_NATIVE_X86_64_SRC_COMPRESS_CONSTS_H
+
+#include "../../../common.h"
+
+#ifndef __ASSEMBLER__
+
+#define mlk_decompress_d4_data MLK_NAMESPACE(decompress_d4_data)
+extern const uint8_t mlk_decompress_d4_data[32];
+
+#endif /* !__ASSEMBLER__ */
+
+#endif /* !MLK_NATIVE_X86_64_SRC_COMPRESS_CONSTS_H */
