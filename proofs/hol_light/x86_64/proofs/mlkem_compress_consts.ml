54114e175214345ec11e41608ed132398335327f
Matthias J. Kannwischer <matthias@kannwischer.eu>
2026-02-02 17:18:28 +0800

    x86_64: Convert poly_decompress_d10_avx2 from intrinsics to assembly
    Cherry-picked from https://github.com/pq-code-package/mlkem-native/pull/1499.
    
    Autogen is adjusted to generate the required constants in compress_consts.c.
    
    Signed-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>

--- proofs/hol_light/x86_64/proofs/mlkem_compress_consts.ml
+++ proofs/hol_light/x86_64/proofs/mlkem_compress_consts.ml
@@ -19,6 +19,14 @@ let decompress_d4_data = define `decompress_d4_data:int list = [
   &6; &6; &6; &6; &7; &7; &7; &7
 ]`;;
 
+(* shufbidx *)
+let decompress_d10_data = define `decompress_d10_data:int list = [
+  &0; &1; &1; &2; &2; &3; &3; &4;
+  &5; &6; &6; &7; &7; &8; &8; &9;
+  &2; &3; &3; &4; &4; &5; &5; &6;
+  &7; &8; &8; &9; &9; &10; &10; &11
+]`;;
+
 (* shufbidx, mask, shift *)
 let decompress_d5_data = define `decompress_d5_data:int list = [
   &0; &0; &0; &1; &1; &1; &1; &2;
