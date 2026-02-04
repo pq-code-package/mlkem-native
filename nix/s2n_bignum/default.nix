caacd162909c0b7d0e45aac64419b91a9be13c90
Matthias J. Kannwischer <matthias@kannwischer.eu>
2026-02-04 12:12:06 +0800

    HOL Light: Add proof sceleton for poly_decompress_d11
    Add HOL Light assembly, bytecode, constants, and proof skeleton for
    poly_decompress_d11_avx2. Update autogen, Makefile, and dump_bytecode
    accordingly.
    
    Switch to s2n-bignum fork adding support for vpsrlvd and vpsrlvq, see
    https://github.com/mkannwischer/s2n-bignum/tree/13cb1dd13989db8b94a98e74cba26dda2a71d7de
    
    Proof itself is left a TODO.
    
    Signed-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>

--- nix/s2n_bignum/default.nix
+++ nix/s2n_bignum/default.nix
@@ -2,12 +2,12 @@
 { stdenv, fetchFromGitHub, writeText, ... }:
 stdenv.mkDerivation rec {
   pname = "s2n_bignum";
-  version = "9b0153c7b6fb7320942967f56e14fb5a7333a0b3";
+  version = "13cb1dd13989db8b94a98e74cba26dda2a71d7de";
   src = fetchFromGitHub {
     owner = "mkannwischer";
     repo = "s2n-bignum";
     rev = "${version}";
-    hash = "sha256-ciVt6T8pGlhrP/FP6jG33/t11hRV3fzhh3YLu0QlhZc=";
+    hash = "sha256-jqF3U7J0hUwM2XJQvvGjje3noEO+snTjLCctsSPadp8=";
   };
   setupHook = writeText "setup-hook.sh" ''
     export S2N_BIGNUM_DIR="$1"
