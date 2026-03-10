5ab8dffeb38cf3446440ac50ba0acc57522f517c
Rod Chapman <rodchap@amazon.com>
2026-03-10 15:42:31 +0000

    Update copyright and licence notices
    Signed-off-by: Rod Chapman <rodchap@amazon.com>

--- proofs/cbmc/invntt_layer321/mlk_invntt_layer321_harness.c
+++ proofs/cbmc/invntt_layer321/mlk_invntt_layer321_harness.c
@@ -1,6 +1,6 @@
 // Copyright (c) 2024-2025 The mlkem-native project authors
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: MIT-0
+// SPDX-License-Identifier: MIT-0 OR ISC OR MIT
 
 #include <poly.h>
 
