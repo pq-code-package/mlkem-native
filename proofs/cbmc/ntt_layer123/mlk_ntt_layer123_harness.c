1aca2ab0f95675dbede665c8729c403ace080616
Rod Chapman <rodchap@amazon.com>
2026-03-10 15:51:01 +0000

    Update copyright and licence notices
    Signed-off-by: Rod Chapman <rodchap@amazon.com>

--- proofs/cbmc/ntt_layer123/mlk_ntt_layer123_harness.c
+++ proofs/cbmc/ntt_layer123/mlk_ntt_layer123_harness.c
@@ -1,4 +1,4 @@
-// Copyright (c) 2024-2025 The mlkem-native project authors
+// Copyright (c) The mlkem-native project authors
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
 
