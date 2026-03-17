856b540bd7dc6154c2828ff9d4cfc61828c62cfd
Matthias J. Kannwischer <matthias@zerorisc.com>
2026-03-17 13:12:42 +0800

    Only compute sp mulcache once
    Signed-off-by: Matthias J. Kannwischer <matthias@zerorisc.com>

--- proofs/cbmc/indcpa_enc_u/indcpa_enc_u_harness.c
+++ proofs/cbmc/indcpa_enc_u/indcpa_enc_u_harness.c
@@ -8,8 +8,9 @@ void harness(void)
   uint8_t *ct_u;
   mlk_polyvec *sp;
   mlk_poly *epp;
+  mlk_polyvec_mulcache *sp_cache;
   uint8_t *seed;
   uint8_t *coins;
-  mlk_indcpa_enc_u(ct_u, sp, epp, seed, coins,
+  mlk_indcpa_enc_u(ct_u, sp, epp, sp_cache, seed, coins,
                    NULL /* context will be dropped by preprocessor */);
 }
--- proofs/cbmc/indcpa_enc_v/indcpa_enc_v_harness.c
+++ proofs/cbmc/indcpa_enc_v/indcpa_enc_v_harness.c
@@ -8,8 +8,9 @@ void harness(void)
   uint8_t *ct_v;
   mlk_polyvec *sp;
   mlk_poly *epp;
+  mlk_polyvec_mulcache *sp_cache;
   uint8_t *m;
   uint8_t *ek_vector;
-  mlk_indcpa_enc_v(ct_v, sp, epp, m, ek_vector,
+  mlk_indcpa_enc_v(ct_v, sp, epp, sp_cache, m, ek_vector,
                    NULL /* context will be dropped by preprocessor */);
 }
