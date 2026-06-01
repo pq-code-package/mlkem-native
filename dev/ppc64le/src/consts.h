cc9087d2e54f0dfeab3ca59f9e37634762e80945
Basil Hess <bhe@zurich.ibm.com>
2026-06-01 10:42:31 +0200

    address hannobecker's comments autogenerate full consts.c and consts.h
    Signed-off-by: Basil Hess <bhe@zurich.ibm.com>
    
    Sync iNTT & Improved ABI note
    
    Signed-off-by: Basil Hess <bhe@zurich.ibm.com>

--- dev/ppc64le/src/consts.h
+++ dev/ppc64le/src/consts.h
@@ -3,12 +3,17 @@
  * SPDX-License-Identifier: Apache-2.0 OR ISC OR MIT
  */
 
+/*
+ * WARNING: This file is auto-generated from scripts/autogen
+ *          in the mlkem-native repository.
+ *          Do not modify it directly.
+ */
+
 #ifndef MLK_DEV_PPC64LE_SRC_CONSTS_H
 #define MLK_DEV_PPC64LE_SRC_CONSTS_H
 #include "../../../common.h"
 
-/* Offsets into the constant table */
-/* check-magic: off */
+/* Byte offsets into the constant table */
 #define MLK_PPC_NQ_OFFSET 0
 #define MLK_PPC_QINV_OFFSET 16
 #define MLK_PPC_Q_OFFSET 32
@@ -20,13 +25,10 @@
 #define MLK_PPC_ZETA_INTT_OFFSET 1120
 #define MLK_PPC_ZETA_NTT_TW_OFFSET 2128
 #define MLK_PPC_ZETA_INTT_TW_OFFSET 3136
-/* check-magic: on */
 
 #ifndef __ASSEMBLER__
 #define mlk_ppc_qdata MLK_NAMESPACE(ppc_qdata)
-/* 7 groups of 8 base constants + 4 twiddle tables * 63 rows * 8 values */
-/* check-magic: 2072 == 7 * 8 + 4 * 63 * 8 */
 MLK_INTERNAL_DATA_DECLARATION const int16_t mlk_ppc_qdata[2072];
-#endif /* !__ASSEMBLER__ */
+#endif
 
 #endif /* !MLK_DEV_PPC64LE_SRC_CONSTS_H */
