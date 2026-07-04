83a68d259fe289e0955454d86e9ed8f7f34b5112
Rod Chapman <rodchap@amazon.com>
2026-07-04 19:17:48 +0100

    Use gcc and minimal libc mock for all CBMC proofs
    1. CBMC Makefile.common now always uses gcc for pre-processing.
    2. Specify -nostdinc for pre-processing, so host-dependent include
       files are NOT used, since this leads to proof instability across
       platforms.
    3. Instead, specify and add our own minimal mock the the standard
       LibC header files, but only what we need for mlkem-native and nothing
       else, all based solely on what is required by ISO C99 (7.17 - 7.21)
    4. First set of header files are for the "LP64" memory model, where
       "Long and Pointers are 64 bits", which is that used by common
       64-bit platforms such as AArch64/Linux, x86_64/Linux and
       AArch64/macOS.
    
    A separate set of header files for "LP32" (for 32-bit targets) will
    follow.
    
    Signed-off-by: Rod Chapman <rodchap@amazon.com>

---
+++ proofs/cbmc/include/LP64/stddef.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) The mlkem-native project authors
+ * SPDX-License-Identifier: Apache-2.0 OR ISC OR MIT
+ */
+
+#ifndef _STDDEF_H
+#define _STDDEF_H 1
+
+typedef unsigned long size_t;
+
+#define NULL (0)
+
+#endif
---
+++ proofs/cbmc/include/LP64/string.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) The mlkem-native project authors
+ * SPDX-License-Identifier: Apache-2.0 OR ISC OR MIT
+ */
+
+#ifndef _STRING_H
+#define _STRING_H 1
+
+#include <stddef.h>
+
+void *memset(void *dest, int ch, size_t count);
+
+void *memcpy(void *dest, const void *src, size_t count);
+
+#endif
