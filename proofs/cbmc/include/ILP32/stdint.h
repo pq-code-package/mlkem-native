54f333a4f34189f53cb3ff127281035b068fb28e
Rod Chapman <rodchap@amazon.com>
2026-07-05 16:27:15 +0100

    Add minimal libc headers for ILP32 ("32-bit") machines.
    Also adds comments explaining how the standard fixed-width
    integer types and SIZE_MAX are defined in each model.
    
    Signed-off-by: Rod Chapman <rodchap@amazon.com>

--- proofs/cbmc/include/ILP32/stdint.h
+++ proofs/cbmc/include/ILP32/stdint.h
@@ -12,24 +12,26 @@ typedef signed char int8_t;
 typedef unsigned short uint16_t;
 typedef short int16_t;
 
-/* "LP64" stands for "Long and Pointer are 64-bit", so
+/* "ILP32" stands for "Int, Long, and Pointer are 32-bit", so
  *   int32_t is "int"
  *  uint32_t is "unsigned int"
- *   int64_t is "long"
- *  uint64_t is "unsigned long"
+ *   int64_t is "long long"
+ *  uint64_t is "unsigned long long"
  * and
- *  SIZE_MAX is 2**64-1
+ *  SIZE_MAX is 2**32-1
  */
 typedef unsigned int uint32_t;
 typedef int int32_t;
 
-typedef unsigned long uint64_t;
-typedef long int64_t;
+typedef unsigned long long uint64_t;
+typedef long long int64_t;
 
-#define SIZE_MAX (18446744073709551615UL)
-#define UINT16_MAX (65535)
+#define SIZE_MAX (4294967295U)
+
+#define UINT16_MAX (65535U)
 #define INT16_MAX (32767)
 #define INT16_MIN (-32767 - 1)
+
 #define INT32_MAX (2147483647)
 #define UINT32_MAX (4294967295U)
 
---
+++ proofs/cbmc/include/ILP32/stdlib.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) The mlkem-native project authors
+ * SPDX-License-Identifier: Apache-2.0 OR ISC OR MIT
+ */
+
+#ifndef _STDLIB_H
+#define _STDLIB_H 1
+
+#include <stddef.h>
+
+void *malloc(size_t size);
+
+void free(void *ptr);
+
+#endif
