59869b580a7b0e1fd1c5b4917710d6d3ed727d44
Matthias J. Kannwischer <matthias@zerorisc.com>
2026-09-14 20:52:35 +0800

    [TEST] nix: Take nixpkgs-unstable from a fork with a fixed ledit
    Signed-off-by: Matthias J. Kannwischer <matthias@zerorisc.com>

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-unstable.url = "github:mkannwischer/nixpkgs/ledit-dontstrip";
 
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
